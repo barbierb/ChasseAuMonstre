76bb36ba7d0c4bf9f90974df770f4572ff86405b
Sylvain <Sylvain@DESKTOP-CIBAHIH>
2019-04-12 19:19:35 +0200

    Ajout des changements de Vincent

--- src/gestionpersonnage/Etoile.java
+++ src/gestionpersonnage/Etoile.java
@@ -1,5 +1,18 @@
 package gestionpersonnage;
 
-public class Etoile extends Item {
-
+public class Etoile extends Item{
+	
+	/**
+	 * Constructeur de la classe Etoile qui définit la variable recupérable à true car une étoile est récupérable
+	 */
+	public Etoile() {
+		this.recuperable=true;
+	}
+	
+	/**
+	 * Méthode toString informant que l'étoile a été récupérée par le joueur
+	 */
+	public String toString() {
+		return "Etoile récupérée";
+	}
 }
