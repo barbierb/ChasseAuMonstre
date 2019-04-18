397e1cb8393a4a34a686606c43714a52c885f25b
Sylvain <Sylvain@DESKTOP-CIBAHIH>
2019-04-18 15:40:55 +0200

    Tests

--- src/gestionpersonnage/Position.java
+++ src/gestionpersonnage/Position.java
@@ -27,18 +27,16 @@ public class Position {
 		return result;
 	}
 
-	@Override
-	public boolean equals(Object obj) {
+	public boolean equals(Position obj) {
 		if (this == obj)
 			return true;
 		if (obj == null)
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Position other = (Position) obj;
-		if (x != other.x)
+		if (x != obj.x)
 			return false;
-		if (y != other.y)
+		if (y != obj.y)
 			return false;
 		return true;
 	}
--- test/testsGestionPersonnage/MainTest.java
+++ test/testsGestionPersonnage/MainTest.java
@@ -1,7 +1,53 @@
 package testsGestionPersonnage;
 
-// je trouve pas les librairies de test #HELPLS
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+import gestionpersonnage.*;
 
 public class MainTest {
 	
+	public Plateau p;
+	public Personnage chasseur,monstre;
+	
+	
+	@BeforeClass
+	public static void beforeAll() {
+		System.out.println("Début des tests");
+	}	
+	@Before
+	public void beforeEach() {
+		System.out.println("Initialisation...");
+		p = new Plateau(10, 10);
+		chasseur = new Chasseur(new Position(0, 0));
+		monstre = new Monstre(new Position(5, 5));
+		p.chasseur=chasseur;
+		p.monstre=monstre;
+		
+		
+		System.out.println("Début du test");
+	}
+
+	@Test
+	public void testPosition() {
+		Position p1 = new Position(0, 0);
+		assertEquals(0,p1.getX());
+		assertEquals(0,p1.getY());
+		assertTrue(p1.equals(p1));
+		assertTrue(new Position(0,0).equals(p1));
+		assertFalse(new Position(1,1).equals(p1));
+	}
+	@Test
+	public void testPersonnage() {
+		assertFalse(chasseur.aEtoile());
+	}
+	@After
+	public void afterEach() {
+		System.out.println("Suivant...");
+	}
+	@AfterClass
+	public static void endTests() {
+		System.out.println("Fin des tests");
+	}
 }
