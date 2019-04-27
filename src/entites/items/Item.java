bfa4568a9998dcc683d26b24c7c95de18761a875
Sylvain <Sylvain@DESKTOP-CIBAHIH>
2019-04-27 17:37:36 +0200

    Javadoc items et longues vues première version

--- src/entites/items/Item.java
+++ src/entites/items/Item.java
@@ -1,9 +1,11 @@
- 
 package entites.items;
 
+/**
+ * Classe abstraite des item du jeu, ayant un attribut récupérable
+ * @author Sylvain
+ */
 public abstract class Item {
 		protected boolean recuperable;
 		
-
 		public abstract String toString();
 }
--- src/entites/items/LongueVue.java
+++ src/entites/items/LongueVue.java
@@ -1,13 +1,27 @@
 package entites.items;
 
-public class LongueVue extends Item{
-	public final int NB_MAX = 5;
+import plateau.Plateau;
+import plateau.Position;
+
+/**
+ * Une longue vue permet au chasseur de voir sur NB_MAX cases 
+ * @author Sylvain
+ *
+ */
+public class LongueVue extends Item {
+	public static int NB_MAX;
+	private static int cptLongueVue;
+	
+	private Position pos;
 	
 	/**
 	 * Constructeur de la classe LongueVue qui définit la variable recupérable à false car une longue vue n'est pas récupérable
 	 */
-	public LongueVue() {
+	public LongueVue(Position p) {
 		this.recuperable=false;
+		LongueVue.NB_MAX = 5;
+		LongueVue.cptLongueVue++;
+		this.pos = p;
 	}
 	
 	/**
@@ -15,5 +29,21 @@ public class LongueVue extends Item{
 	 */
 	public String toString() {
 		return "Monstre détecté";
+	}
+	
+	/**
+	 * Le nombre de longue vues posées
+	 * @return un entier représentant le nombre de longue vues posées
+	 */
+	public int getCptLongueVue() {
+		return cptLongueVue;
+	}
+	
+	/**
+	 * Méthode qui décrémente le nombre de longue vues posées et s'enlève de la liste des items de sa case
+	 */
+	public void supprLongueVue() {
+		Plateau.getInstance().getCase(pos).getDedans().remove(this);
+		cptLongueVue--;
 	}
 }
