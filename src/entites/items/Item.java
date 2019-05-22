7f83d267f80a0e29483e44680fc931f50a2e271d
bretins <bretins@ayou08.iut-infobio.priv.univ-lille1.fr>
2019-05-22 10:27:49 +0200

    Menu principal dégeu

--- src/entites/items/Item.java
+++ src/entites/items/Item.java
@@ -1,11 +1,9 @@
 package entites.items;
 
 /**
- * Classe abstraite des item du jeu, ayant un attribut récupérable
+ * interface des item du jeu
  * @author Sylvain
  */
-public abstract class Item {
-		protected boolean recuperable;
-		
-		public abstract String toString();
+public interface Item {
+	
 }
