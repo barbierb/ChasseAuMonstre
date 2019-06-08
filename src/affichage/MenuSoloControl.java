024512db5f3307551406daac48fb7b84fde2dcc0
ItsPower <34723487+ItsPower@users.noreply.github.com>
2019-06-08 11:56:38 +0200

    menu solo

---
+++ src/affichage/MenuSoloControl.java
@@ -0,0 +1,85 @@
+package affichage;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import reseau.Client;
+import reseau.Serveur;
+
+public class MenuSoloControl {
+
+    @FXML
+    private Pane screen;
+
+    @FXML
+    private ImageView fond;
+
+    @FXML
+    private VBox listeserv;
+
+    @FXML
+    private ImageView monstre;
+
+    @FXML
+    private Text hebergertext;
+
+    @FXML
+    private ImageView chasseur;
+
+    @FXML
+    private Text rejoindretext;
+
+    @FXML
+    private ImageView quitter;
+
+    @FXML
+    public void initialize() {
+    	monstre.setOnMouseClicked(e -> {
+			//Affichage.stage.setScene(null);
+		});
+    	monstre.setOnMouseEntered(e -> {
+    		monstre.setImage(new Image(Affichage.chargerImg("../conteneur_hover.png")));
+		});
+    	monstre.setOnMouseExited(e -> {
+    		monstre.setImage(new Image(Affichage.chargerImg("../conteneur.png")));
+		});
+
+    	chasseur.setOnMouseClicked(e -> {
+			//Client.connecter(ip, Serveur.PORT_JEU);
+		});
+    	chasseur.setOnMouseEntered(e -> {
+    		chasseur.setImage(new Image(Affichage.chargerImg("../conteneur_hover.png")));
+		});
+    	chasseur.setOnMouseExited(e -> {
+    		chasseur.setImage(new Image(Affichage.chargerImg("../conteneur.png")));
+		});
+		quitter.setOnMouseClicked(e->{
+			if(Client.brdTask != null) Client.brdTask.cancel();
+			try {
+				Affichage.getInstance().start(Affichage.stage);
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		});
+		quitter.setOnMouseEntered(e -> {
+			quitter.setImage(new Image(Affichage.chargerImg("../conteneur_hover.png")));
+		});
+		quitter.setOnMouseExited(e -> {
+			quitter.setImage(new Image(Affichage.chargerImg("../conteneur.png")));
+		});
+
+		screen.setOnKeyPressed(e -> {
+			if(!e.getCode().equals(KeyCode.ESCAPE)) return;
+			if(Client.brdTask != null) Client.brdTask.cancel();
+			try {
+				Affichage.getInstance().start(Affichage.stage);
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		});
+    }
+}
--- src/affichage/Menus.java
+++ src/affichage/Menus.java
@@ -2,70 +2,20 @@ package affichage;
 
 import java.io.IOException;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import reseau.Serveur;
 
 public class Menus {
 	
 	protected static Scene getSceneSolo() {
-		BorderPane root = new BorderPane();
-		background(root, Color.GRAY);
-		
-		
-		VBox top = new VBox();
-		root.setTop(top);
-		
-		HBox top0 = new HBox();
-		top.getChildren().add(top0);
-		top0.getChildren().add(boutonRetour());
-		
-		HBox top1 = new HBox();
-		top.getChildren().add(top1);
-		top1.setAlignment(Pos.CENTER);
-		
-		Label solo = new Label("Partie contre l'ordinateur");
-		solo.setFont(getFontTitre());
-		top1.getChildren().add(solo);
-		
-		HBox center = new HBox();
-		root.setCenter(center);
-		center.setAlignment(Pos.CENTER);
-		
-		VBox centergauche = new VBox();
-		centergauche.setAlignment(Pos.CENTER);
-		center.getChildren().add(centergauche);
-		CheckBox monstre = new CheckBox("Jouer en tant que monstre");
-		CheckBox chasseur = new CheckBox("Jouer en tant que chasseur");
-		centergauche.getChildren().addAll(monstre,chasseur);
-		monstre.addEventHandler(ActionEvent.ACTION, e -> {
-			// lancer le jeu en començant comme un monstre
-		});
-		chasseur.addEventHandler(ActionEvent.ACTION, e -> {
-			// lancer le jeu en commençant comme un chasseur
-		});
-		
-		
-		
+		Parent root = null;
+		try {
+			root = FXMLLoader.load(Affichage.class.getResource("menu_solo.fxml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return new Scene(root);
 	}
 	
@@ -111,43 +61,4 @@ public class Menus {
 		}
 		return new Scene(root);
 	}
-		
-	public static void listerFontsConsole() {
-		for(String s: Font.getFontNames()) {
-			System.out.println(s);
-		}
-	}
-	
-	private static void background(Region r,Color c ) {
-		r.setBackground(new Background(new BackgroundFill(
-				c, 
-				new CornerRadii(0), 
-				new Insets(0)))
-				);
-	}
-	
-	private static ImageView boutonRetour() {
-		ImageView retour = new ImageView(new Image("file:img/Back-PNG-Image.png"));
-		retour.setFitWidth(96);
-		retour.setFitHeight(60.6);
-		
-		retour.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
-			try {
-				Affichage.getInstance().start(Affichage.stage);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		});
-		
-		return retour;
-	}
-
-	private static Font getFontTitre(int i)	{
-		return new Font("LobsterTwo",i);
-	}
-	private static Font getFontTitre() {
-		return getFontTitre(35);
-	}
-
-
 }
